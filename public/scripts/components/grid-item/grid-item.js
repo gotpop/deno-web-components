2bb058c46f85eb28f85d022a7aebff3918116b7b
Liam Sobczak <l.sobczak@blexr.com>
2025-03-11 16:08:19 +0000

    Refactor grid-item and site-hero components; remove unused CSS files and update styles for improved organization and maintainability

--- public/scripts/components/grid-item/grid-item.js
+++ public/scripts/components/grid-item/grid-item.js
@@ -1,29 +1,9 @@
-import { loadCSS } from "../../utils/loadCss.js"
-
 export class GridItem extends HTMLElement {
   constructor() {
     super()
-    const shadow = this.attachShadow({ mode: "open" })
-    this.setAttribute("hidden", "")
+  }
 
-    const article = document.createElement("article")
-    const slot = document.createElement("slot")
-
-    const cssPath = new URL("./grid-item.css", import.meta.url).href
-
-    const cssConfig = {
-      cssPath,
-      shadowRoot: shadow,
-      callback: (success) => {
-        if (success) {
-          this.removeAttribute("hidden")
-        }
-      },
-    }
-
-    loadCSS(cssConfig)
-
-    article.appendChild(slot)
-    shadow.appendChild(article)
+  connectedCallback() {
+    console.log("MainContent component mounted")
   }
 }
