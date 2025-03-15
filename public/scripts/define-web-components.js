f17fda9b23d06c55d8034c051e2395d9cf2ae431
Liam Sobczak <l.sobczak@blexr.com>
2025-03-15 19:11:20 +0000

    Add console logging utility and initialize web components in main.js

---
+++ public/scripts/define-web-components.js
@@ -0,0 +1,27 @@
+import { Footer } from "./components/site-footer/site-footer.js"
+import { GridItem } from "./components/grid-item/grid-item.js"
+import { Hero } from "./components/site-hero/site-hero.js"
+import { MainContent } from "./components/main-content/main-content.js"
+import { log } from "./console-log.js"
+
+export const initWebComponents = () => {
+  const components = [
+    ["site-hero", Hero],
+    ["main-content", MainContent],
+    ["grid-item", GridItem],
+    ["site-footer", Footer],
+  ]
+
+  // Register remaining components
+  const definitions = components.map(([name, constructor]) => {
+    if (!customElements.get(name)) {
+      customElements.define(name, constructor)
+      return customElements.whenDefined(name)
+    }
+    return Promise.resolve()
+  })
+
+  Promise.all([
+    ...definitions,
+  ]).then(() => log.success("All components are ready"))
+}
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,36 +1,11 @@
 import { FeatureDetect } from "./feature-detect.js"
-import { Footer } from "./components/site-footer/site-footer.js"
-import { GridItem } from "./components/grid-item/grid-item.js"
-import { Hero } from "./components/site-hero/site-hero.js"
-import { MainContent } from "./components/main-content/main-content.js"
 import { initLiveReload } from "./live-reload.js"
+import { initWebComponents } from "./define-web-components.js"
 
-const components = [
-  ["site-hero", Hero],
-  ["main-content", MainContent],
-  ["grid-item", GridItem],
-  ["site-footer", Footer],
-]
+initWebComponents()
 
-// Register remaining components
-const definitions = components.map(([name, constructor]) => {
-  if (!customElements.get(name)) {
-    customElements.define(name, constructor)
-    return customElements.whenDefined(name)
-  }
-  return Promise.resolve()
-})
+new FeatureDetect(document.getElementById("feature-detect-popover")).init()
 
-Promise.all([
-  ...definitions,
-]).then(() => {
-  console.log("All components are ready")
-})
-
-const dialog = document.getElementById("feature-detect-popover")
-new FeatureDetect(dialog).init()
-
-
-if (window.location.hostname === 'localhost') {
- initLiveReload()
+if (window.location.hostname === "localhost") {
+  initLiveReload()
 }
