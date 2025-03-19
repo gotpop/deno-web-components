d9c57b5e1508b5cecbf9cc8f5e48993a3aa75591
Liam Sobczak <l.sobczak@blexr.com>
2025-03-19 15:11:36 +0000

    Enhance grid layout and styles; update column spans and background properties for improved design consistency

--- src/data/baseFeatures.ts
+++ src/data/baseFeatures.ts
@@ -2,20 +2,20 @@ export const baseFeatures = [
   {
     id: "grid-item-1",
     label: "Title 1",
-    columns: 4,
+    columns: 14,
     rows: 1,
     title: "CSS functions",
     description:
       "CSS functions are a powerful tool for creating dynamic and reusable styles. They allow you to define styles based on the context in which they are used, making it easier to create responsive and flexible designs.",
     titleTag: "h2",
     slug: "css-functions",
-    "feature-start": 2,
-    "feature-end": 5,
+    "feature-start": 4,
+    "feature-end": 11,
   },
   {
     id: "grid-item-2",
     label: "Title 2",
-    columns: 4,
+    columns: 12,
     rows: 1,
     title: "Cross document view transitions",
     description:
@@ -23,12 +23,12 @@ export const baseFeatures = [
     titleTag: "h3",
     slug: "cross-document-view-transitions",
     "feature-start": 2,
-    "feature-end": 5,
+    "feature-end": 10,
   },
   {
     id: "grid-item-3",
     label: "Title 3",
-    columns: 6,
+    columns: 14,
     rows: 1,
     title: "Expanded attr()",
     description:
@@ -36,25 +36,25 @@ export const baseFeatures = [
     titleTag: "h3",
     slug: "expanded-attr",
     "feature-start": 2,
-    "feature-end": 5,
+    "feature-end": 10,
   },
   {
     id: "grid-item-4",
     label: "Title 4",
-    columns: 4,
+    columns: 14,
     rows: 1,
     title: "Anchor positioning",
     description:
       "Anchor positioning is a powerful tool for creating dynamic and reusable styles. They allow you to define styles based on the context in which they are used, making it easier to create responsive and flexible designs",
     titleTag: "h3",
     slug: "anchor-positioning",
-    "feature-start": 2,
-    "feature-end": 5,
+    "feature-start": 4,
+    "feature-end": 11,
   },
   {
     id: "grid-item-5",
     label: "Title 5",
-    columns: 4,
+    columns: 12,
     rows: 1,
     title: "Custom elements",
     description:
@@ -62,12 +62,12 @@ export const baseFeatures = [
     titleTag: "h3",
     slug: "custom-elements",
     "feature-start": 2,
-    "feature-end": 5,
+    "feature-end": 10,
   },
   {
     id: "grid-item-6",
     label: "Title 6",
-    columns: 6,
+    columns: 14,
     rows: 1,
     title: "@scope rule",
     description:
@@ -75,7 +75,7 @@ export const baseFeatures = [
     titleTag: "h3",
     slug: "scope-rule",
     "feature-start": 2,
-    "feature-end": 5,
+    "feature-end": 10,
   },
 ]
 
