f556321e636e126dba96136ccc1f221496d69d50
Liam Sobczak <l.sobczak@blexr.com>
2025-03-21 15:18:35 +0000

    Refactor browser API feature definitions to enhance clarity and structure by updating descriptions, layout properties, and ensuring consistent slug and title usage

--- src/data/features/browser-api.ts
+++ src/data/features/browser-api.ts
@@ -2,38 +2,35 @@ import { Feature } from "./data-types.ts"
 
 export const browserApiFeaturesMap = new Map()
   .set("css-view-transitions-2", {
-    tags: ["browser-api"],
-    layout: {
-      columns: 12,
-      rows: 1,
-      "feature-start": 2,
-      "feature-end": 10,
-    },
-    title: "Cross document view transitions",
     description:
       "Cross-document view transitions are a powerful tool for creating dynamic and reusable styles. They allow you to define styles based on the context in which they are used, making it easier to create responsive and flexible designs",
-    slug: "css-view-transitions-2",
+    layout: {
+      columns: 12,
+      "feature-end": 10,
+      "feature-start": 2,
+      rows: 1,
+    },
     links: [
       {
         label: "W3C Specification",
-        link: "https://www.w3.org/TR/css-view-transitions-2/",
+        link: "https://www.w3.org/TR/css-view-transitions-2",
         type: "spec",
       },
     ],
+    slug: "css-view-transitions-2",
+    tags: ["browser-api"],
+    title: "Cross document view transitions",
+    type: "bapi",
   })
   .set("custom-elements", {
-    tags: ["browser-api"],
-    layout: {
-      columns: 12,
-      rows: 1,
-      "feature-start": 2,
-      "feature-end": 10,
-    },
-    label: "Title 5",
-    title: "Custom elements",
     description:
       "Custom elements are a powerful tool for creating dynamic and reusable styles. They allow you to define styles based on the context in which they are used, making it easier to create responsive and flexible designs",
-    slug: "custom-elements",
+    layout: {
+      columns: 12,
+      "feature-end": 10,
+      "feature-start": 2,
+      rows: 1,
+    },
     links: [
       {
         label: "MDN Documentation",
@@ -47,6 +44,10 @@ export const browserApiFeaturesMap = new Map()
         type: "github",
       },
     ],
+    slug: "custom-elements",
+    tags: ["browser-api"],
+    title: "Custom elements",
+    type: "bapi",
   })
 
 export const browserApiFeatures: Feature[] = Array.from(
