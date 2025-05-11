2ab7fc456052e186565b49001d8c9c917b535ae7
Liam Sobczak <l.sobczak@blexr.com>
2025-05-11 15:13:27 +0100

    Add navigation data export and refactor nav template to use it for improved maintainability

---
+++ src/data/navigation.ts
@@ -0,0 +1,15 @@
+// Navigation menu items
+export const navigationData = {
+  menuItems: [
+    { name: "Home", href: "/", key: "index" },
+    { name: "About", href: "/about", key: "about" },
+    { name: "Contact", href: "/contact", key: "contact" },
+    { name: "Features", href: "/features", key: "features" },
+    {
+      name: "GitHub",
+      href: "https://github.com/gotpop/deno-web-components",
+      key: "docs",
+      isExternal: true,
+    },
+  ],
+}
