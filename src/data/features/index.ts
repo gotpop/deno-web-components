a6f6f275e2f8551c9c6ab16039ccde314d7456c6
Liam Sobczak <l.sobczak@blexr.com>
2025-05-26 14:33:21 +0100

    Refactor feature imports in home.ts to use allFeatures and filter by tags

--- src/data/features/index.ts
+++ src/data/features/index.ts
@@ -2,10 +2,6 @@ import { browserApiFeatures } from "./browser-api.ts"
 import { cssFeatures } from "./css.ts"
 import { cssomFeatures } from "./css-object-model.ts"
 
-export * from "./browser-api.ts"
-export * from "./css.ts"
-export * from "./css-object-model.ts"
-
 export const allFeatures = [
   ...browserApiFeatures,
   ...cssFeatures,
--- src/data/home.ts
+++ src/data/home.ts
@@ -1,14 +1,22 @@
-import {
-  browserApiFeatures,
-  cssFeatures,
-  cssomFeatures,
-} from "./features/index.ts"
+import { allFeatures } from "./features/index.ts"
+
+const filteredFeaturesBrowserApi = allFeatures.filter((feature) =>
+  feature.tags.includes("browser-api")
+)
+
+const filteredFeaturesCss = allFeatures.filter((feature) =>
+  feature.tags.includes("css")
+)
+
+const filteredFeaturesCssom = allFeatures.filter((feature) =>
+  feature.tags.includes("cssom")
+)
 
 export const homeData = {
   features: {
-    browserApiFeatures,
-    cssFeatures,
-    cssomFeatures,
+    browserApiFeatures: filteredFeaturesBrowserApi,
+    cssFeatures: filteredFeaturesCss,
+    cssomFeatures: filteredFeaturesCssom,
   },
   bodyMarkupData: [
     {
