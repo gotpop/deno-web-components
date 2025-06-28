9ee9d476cebf8785a76862c933bc6b74c9ef779e
GotPop <liam@gotpop.net>
2025-06-28 18:24:14 +0100

    Refactor order change handling to improve readability and maintainability

--- public/scripts/components/order-select.js
+++ public/scripts/components/order-select.js
@@ -1,32 +1,35 @@
+function handleOrderChange(selectElement, viewTransitionManager) {
+  const url = new URL(window.location.href)
+  const params = new URLSearchParams(url.search)
+
+  if (selectElement.value) {
+    params.set(selectElement.name, selectElement.value)
+  } else {
+    params.delete(selectElement.name)
+  }
+
+  const newUrl = new URL(
+    `${url.pathname}?${params.toString()}`,
+    window.location.origin,
+  )
+  
+  console.log("[order-select] Order select changed. Navigating to:", newUrl.href)
+
+  if (viewTransitionManager && viewTransitionManager.navigate) {
+    viewTransitionManager.navigate(newUrl)
+  } else {
+    window.location.href = newUrl.href
+  }
+}
+
 export function initializeOrderSelect(viewTransitionManager) {
   const orderSelect = document.getElementById("order")
 
-
   if (orderSelect) {
     orderSelect.addEventListener("change", function () {
-      const url = new URL(window.location.href)
-      const params = new URLSearchParams(url.search)
-
-      if (this.value) {
-        params.set(this.name, this.value)
-      } else {
-        params.delete(this.name)
-      }
-
-      const newUrl = new URL(
-        `${url.pathname}?${params.toString()}`,
-        window.location.origin,
-      )
-      
-      console.log("[main.js] Order select changed. Navigating to:", newUrl.href)
-
-      if (viewTransitionManager && viewTransitionManager.navigate) {
-        viewTransitionManager.navigate(newUrl)
-      } else {
-        window.location.href = newUrl.href
-      }
+      handleOrderChange(this, viewTransitionManager)
     })
   } else {
-    console.log("[main.js] Order select element not found on this page.")
+    console.log("[order-select] Order select element not found on this page.")
   }
 }
